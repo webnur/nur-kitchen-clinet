6daf09c424c7a11c139b815f04ff463464770a74
webnur <mdnuruli291@gmai.com>
2022-11-10 18:30:27 +0600

    review data added alhamdulillah

---
+++ src/components/Review/Review.js
@@ -0,0 +1,24 @@
+import React from 'react';
+const Review = ({review}) => {
+    console.log(review)
+    return (
+        <tbody>
+        <tr className="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600">
+            <th scope="row" className="py-4 px-6 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                {review.comment}
+            </th>
+            <td className="py-4 px-6">
+               {review.author}
+            </td>
+            <td className="py-4 px-6">
+            <img className="w-12 h-12 rounded-lg" src={review.image} alt="Default avatar"></img>
+            </td>
+            <td className="py-4 px-6 text-right">
+                <button className="font-medium text-blue-600 dark:text-blue-500 hover:underline">Delete</button>
+            </td>
+        </tr>
+    </tbody>
+    );
+};
+
+export default Review;
--- src/pages/MyReviews/MyReviews.js
+++ src/pages/MyReviews/MyReviews.js
@@ -1,9 +1,47 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import Review from '../../components/Review/Review';
 
 const MyReviews = () => {
+    const [reviews, setReviews] = useState([])
+    useEffect(() => {
+        fetch('http://localhost:5000/reviews')
+            .then(res => res.json())
+            .then(data => {
+                setReviews(data)
+            })
+    }, [])
+
+
     return (
         <div>
-            <h2>Welcome to My Reviews</h2>
+            <h2 className='text-3xl text-center my-10'> My Reviews</h2>
+
+
+            <div className="overflow-x-auto relative shadow-md sm:rounded-lg container mx-auto">
+                <table className="w-full text-sm text-left text-gray-500 dark:text-gray-400">
+                    <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+                        <tr>
+                            <th scope="col" className="py-3 px-6">
+                                Reviews
+                            </th>
+                            <th scope="col" className="py-3 px-6">
+                                Name
+                            </th>
+                            <th scope="col" className="py-3 px-6">
+                                Author Image
+                            </th>
+                            <th scope="col" className="py-3 px-6">
+                                <span className="sr-only">Edit</span>
+                            </th>
+                        </tr>
+                    </thead>
+                    {
+                        reviews.map(review => <Review key={review._id} review={review}></Review>)
+                    }
+
+                </table>
+            </div>
+
         </div>
     );
 };
